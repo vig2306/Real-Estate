c2db11579a0dfd9d0de1d9f511960c621687cfb5
Parth Shah <pns1@somaiya.edu>
2020-12-02 15:42:45 +0530

    Spam Contacts Bug Removed

--- contacts/views.py
+++ contacts/views.py
@@ -13,6 +13,13 @@ def contact(request):
         user_id = request.POST["user_id"]
         realtor_email = request.POST["realtor_email"]
 
+        if (request.user.is_authenticated):
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, "An Inquiry has already been made.Please do not Spam")
+                return redirect('/listings/' + listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         print(listing)
         contact.save()
